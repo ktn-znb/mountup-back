3e8c08aa2bf9b51010e0e8def75c370f804943e6
Zainab Khatoon <ktn.znb@gmail.com>
2024-11-28 23:55:51 +0400

    Added the mongo connection

--- server.js
+++ server.js
@@ -5,15 +5,30 @@ app.use(express.json());
 app.set("port", 3000);
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    next();
-  });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  next();
+});
+
+const MongoClient = require("mongodb").MongoClient;
+let db;
+
+MongoClient.connect(
+  "mongodb+srv://zainabkhatoon3104:rootpassword@cluster0.dfc2p.mongodb.net/",
+  (err, client) => {
+    if (err) {
+      console.error("Database connection failed:", err.message);
+      return;
+    }
+    db = client.db("mountup");
+    console.log("Database connected successfully");
+  }
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
