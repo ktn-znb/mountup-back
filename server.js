5b61913a7150be1ae9fd5cb69fb923766b732f4d
Zainab Khatoon <ktn.znb@gmail.com>
2024-12-02 15:47:06 +0400

    Instead of writing and maintaining multiple cors headers manually i installed the cors library

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 require("dotenv").config();
 
 app.use(express.json());
 app.set("port", 3000);
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  next();
-});
+app.use(cors());
 
 const MongoClient = require("mongodb").MongoClient;
 let db;
@@ -69,7 +61,6 @@ app.put("/collection/activities/:id", (req, res, next) => {
 });
 
 app.get("/collection/activities/search", (req, res, next) => {
-  console.log("Do you get here?");
   const searchQuery = req.query.q;
 
   if (!searchQuery) {
